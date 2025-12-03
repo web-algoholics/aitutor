592cefa4f89efc66abff2f732d87fca84ecb9482
bakamol <kamoljon2004mm@gmail.com>
2025-12-03 13:24:06 +0300

    Updated brojsconfig

--- bro.config.js
+++ bro.config.js
@@ -1,11 +1,18 @@
 const pkg = require('./package')
+const webpack = require('webpack')
 
 module.exports = {
   apiPath: 'stubs/api',
   webpackConfig: {
     output: {
       publicPath: `/static/${pkg.name}/${process.env.VERSION || pkg.version}/`
-    }
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.REACT_APP_API_URL': JSON.stringify(process.env.REACT_APP_API_URL || 'http://localhost:8000'),
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+      }),
+    ],
   },
   /* use https://admin.bro-js.ru/ to create config, navigations and features */
   navigations: {
